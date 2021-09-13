6c0d31dc3be3ba8746712bc0fe6f5cc3223ab8c0
fadhilthomas <mthomasfadhilayahya@gmail.com>
2021-09-13 21:43:29 +0000

    feat: add open vulnerability summary

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"github.com/fadhilthomas/go-nuclei-slack/config"
 	"github.com/rs/zerolog/log"
 	"net/http"
@@ -18,6 +19,7 @@ type SlackRequestBody struct {
 	Title       string                `json:"title"`
 	Text        string                `json:"text"`
 	Attachments []SlackAttachmentBody `json:"attachments"`
+	Blocks      []SlackBlockBody      `json:"blocks"`
 }
 
 type SlackAttachmentBody struct {
@@ -25,10 +27,20 @@ type SlackAttachmentBody struct {
 	Fields []SlackFieldBody `json:"fields"`
 }
 
+type SlackBlockBody struct {
+	Type string              `json:"type"`
+	Text SlackBlockFieldBody `json:"text"`
+}
+
 type SlackFieldBody struct {
 	Title string `json:"title"`
 	Value string `json:"value"`
 	Short bool   `json:"short"`
+}
+
+type SlackBlockFieldBody struct {
+	Type string `json:"type"`
+	Text string `json:"text"`
 }
 
 type Output struct {
@@ -53,10 +65,20 @@ type Output struct {
 	Timestamp time.Time `json:"timestamp"`
 }
 
+type SummaryReport struct {
+	Critical int
+	High     int
+	Medium   int
+	Low      int
+	Info     int
+}
+
 func main() {
 	config.Set(config.LOG_LEVEL, "info")
 	webHookURL := config.GetStr(config.SLACK_TOKEN)
 	var attachmentList []SlackAttachmentBody
+	var blockList []SlackBlockBody
+	summary := SummaryReport{}
 
 	file, err := os.Open(config.GetStr(config.FILE_LOCATION))
 	if err != nil {
@@ -69,32 +91,34 @@ func main() {
 		if err != nil {
 			log.Error().Str("file", "slack").Err(err)
 		}
-		attachmentList = append(attachmentList, createAttachment(result.TemplateID, result.Info.Name, result.Info.Author[0], strings.Join(result.Info.Tags, ", "), result.Info.Severity, result.Info.Classification.CvssMetrics, strconv.FormatFloat(result.Info.Classification.CvssScore, 'f', -1, 64), result.Host, result.Matched, result.IP))
+
+		switch result.Info.Severity {
+		case "critical":
+			summary.Critical++
+		case "high":
+			summary.High++
+		case "medium":
+			summary.Medium++
+		case "low":
+			summary.Low++
+		case "info":
+			summary.Info++
+		}
+		attachmentList = append(attachmentList, createAttachment(result.Info.Name, strings.Join(result.Info.Tags, ", "), result.Info.Severity, result.Info.Classification.CvssMetrics, strconv.FormatFloat(result.Info.Classification.CvssScore, 'f', -1, 64), result.Host, result.Matched))
 	}
 
-	err = sendSlackNotification(webHookURL, attachmentList)
+	blockList = append(blockList, createBlock(summary))
+	err = sendSlackNotification(webHookURL, attachmentList, blockList)
 	if err != nil {
 		log.Error().Str("file", "main").Err(err)
 	}
 }
 
 //nolint:funlen
-func createAttachment(templateID string, name string, author string, tags string, severity string, metric string, score string, host string, matched string, ip string) (attachment SlackAttachmentBody) {
-	templateIDField := SlackFieldBody{
-		Title: "Template ID",
-		Value: templateID,
-		Short: true,
-	}
-
+func createAttachment(name string, tags string, severity string, metric string, score string, host string, matched string) (attachment SlackAttachmentBody) {
 	nameField := SlackFieldBody{
 		Title: "Name",
-		Value: name,
-		Short: true,
-	}
-
-	authorField := SlackFieldBody{
-		Title: "Author",
-		Value: author,
+		Value: fmt.Sprintf("`%s`", name),
 		Short: true,
 	}
 
@@ -106,14 +130,8 @@ func createAttachment(templateID string, name string, author string, tags string
 
 	metricField := SlackFieldBody{
 		Title: "CVSS Metric",
-		Value: metric,
+		Value: fmt.Sprintf("%s - %s", score, metric),
 		Short: false,
-	}
-
-	scoreField := SlackFieldBody{
-		Title: "CVSS Score",
-		Value: score,
-		Short: true,
 	}
 
 	hostField := SlackFieldBody{
@@ -134,12 +152,6 @@ func createAttachment(templateID string, name string, author string, tags string
 		Short: true,
 	}
 
-	ipField := SlackFieldBody{
-		Title: "IP Address",
-		Value: ip,
-		Short: true,
-	}
-
 	var color string
 	switch {
 	case severity == "critical" || severity == "high":
@@ -151,7 +163,7 @@ func createAttachment(templateID string, name string, author string, tags string
 	}
 
 	var fieldList []SlackFieldBody
-	fieldList = append(fieldList, templateIDField, nameField, authorField, tagsField, hostField, ipField, scoreField, severityField, metricField, matchedField)
+	fieldList = append(fieldList, nameField, tagsField, severityField, hostField, metricField, matchedField)
 
 	attachment = SlackAttachmentBody{
 		Fields: fieldList,
@@ -160,14 +172,31 @@ func createAttachment(templateID string, name string, author string, tags string
 	return attachment
 }
 
-func sendSlackNotification(webHookURL string, attachmentList []SlackAttachmentBody) error {
+func createBlock(summary SummaryReport) (block SlackBlockBody) {
+	summaryField := SlackBlockFieldBody{
+		Type: "mrkdwn",
+		Text: fmt.Sprintf("> *Open Vulnerability Summary*\n```Severity      Count\n-------------------\nCritical      %d\nHigh          %d\nMedium        %d\nLow           %d\nInfo          %d\n-------------------\nTotal         %d```", summary.Critical, summary.High, summary.Medium, summary.Low, summary.Info, summary.Critical+summary.High+summary.Medium+summary.Low+summary.Info),
+	}
+
+	block = SlackBlockBody{
+		Type: "section",
+		Text: summaryField,
+	}
+	return block
+}
+
+func sendSlackNotification(webHookURL string, attachmentList []SlackAttachmentBody, blockList []SlackBlockBody) error {
 	slackMessage := SlackRequestBody{
 		Title:       "Penetration Testing Report - Open Vulnerability",
 		Text:        "Penetration Testing Report - Open Vulnerability",
 		Attachments: attachmentList,
+		Blocks:      blockList,
 	}
 
-	slackBody, _ := json.Marshal(slackMessage)
+	slackBody, err := json.Marshal(slackMessage)
+	if err != nil {
+		return err
+	}
 
 	req, err := http.NewRequest(http.MethodPost, webHookURL, bytes.NewBuffer(slackBody))
 	if err != nil {
@@ -177,13 +206,15 @@ func sendSlackNotification(webHookURL string, attachmentList []SlackAttachmentBo
 	req.Header.Add("Content-Type", "application/json")
 	client := &http.Client{Timeout: 10 * time.Second}
 	resp, err := client.Do(req) //nolint:bodyclose
-
 	if err != nil {
 		return err
 	}
 
 	buf := new(bytes.Buffer)
-	_, _ = buf.ReadFrom(resp.Body)
+	_, err = buf.ReadFrom(resp.Body)
+	if err != nil {
+		return err
+	}
 	log.Debug().Str("file", "slack").Msg(buf.String())
 	if buf.String() != "ok" {
 		return errors.New("non-ok response returned from slack")
